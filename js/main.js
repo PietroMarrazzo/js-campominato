ae4d4c58d7bfe56b05f887a08e7855794ddb73f7
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-23 14:13:51 +0200

    generatore mine + array works

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 // generazione 16 mine, numeri casuali intervallo 1-100, compresi
 var difficult = 100;
-var mine = 16;
+var maxMine = 16;
 var valoriMine = [];
 
 // array mine
-for (i=0; i <= mine; i++) {
+do {
     // valoriMine.push(generaMine(1, 16));
-    mina = generaMine(1, 16);
+    var mina = generaMine(1, 100);
     if (! valoriMine.includes(mina)) {
         valoriMine.push(mina);
     }
-}
+} while (valoriMine.length != maxMine);
 console.log(valoriMine);
 
 // FUNZIONI
 // funzione genera mine 
 function generaMine(min, max) {
-    var valoriMine = [];
-    for (var i = 0; i <= max; i++) {
+    for (var i = 0; i <= maxMine; i++) {
         var mina = Math.floor((Math.random() * max) + min );
         return mina;   
     }
